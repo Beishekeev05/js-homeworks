ce56c64f991db8cc990ed206e589adef6597c81d
Syimyk Beishekeev <syjmyk907@gmail.com>
2024-02-17 18:05:09 +0600

    delete homework-1

--- hw-2/js.js
+++ hw-2/js.js
@@ -1,4 +1,68 @@
 console.log("Меня зовут");
-console.log(`Меня зовут: Сыймык`)
-console.log(`Общая стоимость:${5 + 3}$`)
-console.log(``)
+
+// const NurtName = prompt("Введите ваше имя");
+
+// alert("Вы успешно зарегистрировались!");
+
+// let isBoss = confirm(
+//   "При перезагрузке сайта, введённые данные на форме будут очищены, Вы уверены что хотите перезагрузить?"
+// );
+
+console.log(`Общая стоимость:${5 + 3}`);
+
+let number = 599;
+
+let numberOne = 295;
+
+let x = number + numberOne;
+
+let c = 100;
+
+console.log(x / c);
+
+let numbir = 599;
+
+let num = 295;
+
+let border = numbir + num;
+
+let division = 100;
+
+console.log(
+  `Общая стоимость:${
+    border / division
+  } доллара. Спасибо за покупку приходите еще!`
+);
+let element = 2 * 2095;
+
+let elementtwo = 2 * 799;
+
+let totalFirst = element + elementtwo;
+
+let divisionTwo = 100;
+
+console.log(`$${totalFirst / divisionTwo}`);
+
+let delivery = 499;
+
+let deliverys = 499;
+
+let total = delivery + deliverys;
+
+let divisionThree = 100;
+
+console.log(`Доставка и отправка:$${total + divisionThree}`);
+//---------------------------- таблица умножение до 9 ------------------------//
+
+// const namepromk = prompt("Напишите одну цифру");
+
+// console.log(`1*${namepromk} =${1 * namepromk}`);
+// console.log(`2*${namepromk} =${2 * namepromk}`);
+// console.log(`3*${namepromk} =${3 * namepromk}`);
+// console.log(`4*${namepromk} =${4 * namepromk}`);
+// console.log(`5*${namepromk} =${5 * namepromk}`);
+// console.log(`6*${namepromk} =${6 * namepromk}`);
+// console.log(`7*${namepromk} =${7 * namepromk}`);
+// console.log(`8*${namepromk} =${8 * namepromk}`);
+// console.log(`9*${namepromk} =${9 * namepromk}`);
+// console.log(`10*${namepromk} =${10 * namepromk}`);
