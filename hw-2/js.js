e66cd811b9dcdd0e52c3fc5dc4f6740de054dc02
Syimyk Beishekeev <syjmyk907@gmail.com>
2024-02-18 16:35:56 +0600

    and home work

--- hw-2/js.js
+++ hw-2/js.js
@@ -1,79 +1,97 @@
-// console.log("Меня зовут");
+console.log("Меня зовут");
 
-// const NurtName = prompt("Введите ваше имя");
+const NurtName = prompt("Введите ваше имя");
 
-// alert("Вы успешно зарегистрировались!");
+alert("Вы успешно зарегистрировались!");
 
-// let isBoss = confirm(
-//   "При перезагрузке сайта, введённые данные на форме будут очищены, Вы уверены что хотите перезагрузить?"
-// );
+let isBoss = confirm(
+  "При перезагрузке сайта, введённые данные на форме будут очищены, Вы уверены что хотите перезагрузить?"
+);
 
-// console.log(`Общая стоимость: $${5 + 3}`);
+console.log(`Общая стоимость: $${5 + 3}`);
 
-// let number = 599;
+let number = 599;
 
-// let numberOne = 299;
+let numberOne = 299;
 
-// let x = (599 + 299);
+let x = 599 + 295;
 
-// let c = 100;
+let c = 100;
 
-// console.log(x / c);
+console.log(x / c);
 
-// let numbir = 599;
+let numbir = 599;
 
-// let num = 295;
+let num = 295;
 
-// let border = numbir + num;
+let border = numbir + num;
 
-// let division = 100;
+let division = 100;
 
-// console.log(
-//   `Общая стоимость:${
-//     border / division
-//   } доллара. Спасибо за покупку приходите еще!`
-// );
+console.log(
+  `Общая стоимость:${border / division
+  } доллара. Спасибо за покупку приходите еще!`
+);
 
-// let hope = 2 * 2095 + 2 * 799;
+let numb = 2;
+let numbe = 848;
+let numbers = 2;
+let mumber = 799;
+let total = numb * numbe + numbers * mumber;
+let divisionTwo = 100;
+let totaliti = total / divisionTwo;
+console.log(`Элементы (4): $${totaliti}`);
 
-// let divisionTwo = 100;
+let shipping = 499;
+let handling = 499;
+let divisionThree = 100;
+let totall = 499 * 2;
+let result = totall / divisionThree;
+console.log(`Shipping & handling: $${result}`);
 
-// console.log(` ${hope / divisionTwo}`);
-// //
+let taxi = ((2 * 2095) + (2 * 799))
 
-// let delivery = 499;
+let divisionFour = 100
 
-// let deliverys = 499;
+console.log(`Total before tax:$${taxi / divisionFour}`);
 
-// let total = delivery + deliverys;
+console.log(`Estimated tax (10%): $${Math.round((2 * 2095 + 2 * 799 + 499 + 499) * 0.1) / 100}`);
 
-// let divisionThree = 100;
+const name = 'Simon'
 
-// console.log(`Доставка и отправка:$${total + divisionThree}`);
-//---------------------------- таблица умножение до 9 ------------------------//
+console.log(`My name is:${name}`)
 
-// const namepromk = prompt("Напишите одну цифру");
+console.log(
+  Math.round(5 + 2 * 3 + 9)
+)
 
-// console.log(`1*${namepromk} =${1 * namepromk}`);
-// console.log(`2*${namepromk} =${2 * namepromk}`);
-// console.log(`3*${namepromk} =${3 * namepromk}`);
-// console.log(`4*${namepromk} =${4 * namepromk}`);
-// console.log(`5*${namepromk} =${5 * namepromk}`);
-// console.log(`6*${namepromk} =${6 * namepromk}`);
-// console.log(`7*${namepromk} =${7 * namepromk}`);
-// console.log(`8*${namepromk} =${8 * namepromk}`);
-// console.log(`9*${namepromk} =${9 * namepromk}`);
-// console.log(`10*${namepromk} =${10 * namepromk}`);
+let coste = 16;
 
+console.log(`Cost of food:$${coste}`)
 
+let tax = 16;
 
+let cost = 0.1;
 
+console.log(`${tax * cost}`)
 
+const totalCost = tax + cost;
 
+console.log(`Total cost: $${totalCost}`);
 
+let card = 0;
 
+// ---------------------------- таблица умножение до 9 ------------------------//
 
+const namepromk = prompt("Напишите одну цифру");
 
-
- 
-let 
+console.log(`1*${namepromk} =${1 * namepromk}`);
+console.log(`2*${namepromk} =${2 * namepromk}`);
+console.log(`3*${namepromk} =${3 * namepromk}`);
+console.log(`4*${namepromk} =${4 * namepromk}`);
+console.log(`5*${namepromk} =${5 * namepromk}`);
+console.log(`6*${namepromk} =${6 * namepromk}`);
+console.log(`7*${namepromk} =${7 * namepromk}`);
+console.log(`8*${namepromk} =${8 * namepromk}`);
+console.log(`9*${namepromk} =${9 * namepromk}`);
+console.log(`10*${namepromk} =${10 * namepromk}`);
